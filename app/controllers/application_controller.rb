71c2518b8296c8c58bda8b40acc4385b2ec434e1
devMunyi <samunyi90@gmail.com>
2023-02-15 16:39:42 +0300

    Update controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # to whitelist additional input fields other than what devise has by default
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
