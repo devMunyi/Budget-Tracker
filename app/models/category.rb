799644e45a237a9cf9702d46d327029d7fba73d3
devMunyi <samunyi90@gmail.com>
2023-02-15 16:38:03 +0300

    Create models

---
+++ app/models/category.rb
@@ -0,0 +1,13 @@
+class Category < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :category_expenses, dependent: :destroy
+  has_many :expenses, through: :category_expenses, dependent: :destroy
+
+  # validations
+  validates :name, presence: true
+  validates :icon, presence: true
+
+  def format_date(time_with_zone)
+    time_with_zone.strftime('%d %b %Y')
+  end
+end
